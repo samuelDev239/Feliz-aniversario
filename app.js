8baa212f7da132701f27739df49b1aa5f99cf431
samuelDev239 <115513489+samuelDev239@users.noreply.github.com>
2022-11-22 20:16:45 -0300

    new

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const nav = document.querySelector('.navbar')
 const btnzin = document.querySelector('.btn-menu')
 const menu = document.querySelector('.menu')
+const botao = document.querySelector('.btn-container')
 function depois(event) {
     if (event.type === "touchstart") event.preventDefault()
     nav.classList.toggle("active")
 }
+function change(event){
+    if (event.type === "touchstart") event.preventDefault()
+    botao.classList.toggle("active")
+
+}
 
 btnzin.addEventListener("click", depois)
 btnzin.addEventListener("touchstart", depois)
+botao.addEventListener("click", change)
+botao.addEventListener("touchstart", change)
